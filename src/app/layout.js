fd74579de030352ad368aa5159c8263bdd5782ec
Josh Michael Olea <34050740+joshmichael23@users.noreply.github.com>
2024-01-11 00:19:21 +0800

    2024-01-11 test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Root from './Root/Root.jsx'
+import Root from './Root/root'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +13,7 @@ export default function RootLayout({children}) {
   return (
     <html lang="en">
       <body>
-        <>
-          <Root></Root>
-        </>
+        <Root></Root>
       </body>
     </html>
   )
